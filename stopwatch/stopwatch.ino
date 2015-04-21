044dd0d0dfb7a7f634c920590b70ecaf002769d1
Austin Jackson <themacphage@gmail.com>
2015-04-21 10:24:25 -0500

    Testing stopwatch circuit
    This is a pain in the ass

--- stopwatch/stopwatch.ino
+++ stopwatch/stopwatch.ino
@@ -1,83 +1,39 @@
-int data = 6; 
-int latch = 8; 
-int clock = 10;
-int answer = 0;
-int start = 7;
-int com15 = 3;
+#define LATCH 10
+#define CLOCK 8
+#define DATA 6
+#define ALL B11111111
+#define NONE B00000000
 
+const int digits[] = {3, 159, 37, 13, 153, 73, 65, 31, 1, 25};
+const int t = 12;
 
 void setup()
 {
   Serial.begin(9600);
-  randomSeed(analogRead(0));
-  Serial.flush();
-  pinMode(data,OUTPUT);
-  pinMode(latch,OUTPUT);
-  pinMode(clock,OUTPUT);
-  pinMode(start,INPUT);
-  pinMode(com15,OUTPUT);
-
+  pinMode(DATA, OUTPUT);
+  pinMode(LATCH, OUTPUT);
+  pinMode(CLOCK, OUTPUT);
+  pinMode(t, INPUT);
 }
-
-int Displayi(int num){
-  int t [] = {
-  ,37,159,3                           };
-  if(num < 10){
-    digitalWrite(latch, LOW);    //prepare for shift
-    shiftOut(data, clock, LSBFIRST,t[num] );
-    shiftOut(data, clock, LSBFIRST,t[0] );    
-    digitalWrite(latch, HIGH);   //shift the bits
-
-  }
-  else if(num >= 10)
-  {
-    int left = num%10;
-    int right = num/10;
-    digitalWrite(latch, LOW);    //prepare for shift
-    shiftOut(data, clock, LSBFIRST,t[left] );
-    shiftOut(data, clock, LSBFIRST,t[right] );
-    digitalWrite(latch, HIGH);   //shift the bits
-
-  }
-}
-boolean state = false;
 
 void loop()
-{ 
-
-    for (int i = 60; i> 0; i--)
-    {
-      
-      //Displayi(i);
-      digitalWrite(latch, LOW);    //prepare for shift
-    shiftOut(data, clock, LSBFIRST,B00000000 );
-    shiftOut(data, clock, LSBFIRST,B00000000 );
-    digitalWrite(latch, HIGH);   //shift the bits
-      delay(1000);
-      
-      
-    }
-
+{
+  for (int i = 0; i < 60; i++)
+  {
+    Serial.println("yes");
+    digitalWrite(LATCH, LOW);
+    shiftOut(DATA, CLOCK, LSBFIRST, B00000000);
+    shiftOut(DATA, CLOCK, LSBFIRST, B00000000);
+    digitalWrite(LATCH, HIGH);
+    delay(1000);
   }
 
+//  digitalWrite(LATCH, LOW);
+//  shiftOut(DATA, CLOCK, LSBFIRST, digits[i / 10]);
+//  shiftOut(DATA, CLOCK, LSBFIRST, digits[i % 10]);
+//  digitalWrite(LATCH, HIGH);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
