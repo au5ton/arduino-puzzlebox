747c29404acd43568f083b45716c85f012f24f38
Austin Jackson <themacphage@gmail.com>
2015-04-27 10:33:27 -0500

    More comments

--- stopwatch/stopwatch.ino
+++ stopwatch/stopwatch.ino
@@ -16,19 +16,12 @@
 //These let us display Englsih alphabet looking numbers to the 7-segment display
 //
 const int digits[] = {3, 159, 37, 13, 153, 73, 65, 31, 1, 25};
-
-void displayNumber(int i) {
-  digitalWrite(LATCH, LOW);
-  shiftOut(DATA, CLOCK, LSBFIRST, digits[i % 10]);
-  shiftOut(DATA, CLOCK, LSBFIRST, digits[i / 10]);
-  digitalWrite(LATCH, HIGH);
-}
-
 boolean gameWon = false;
-
+const int timerLength = 60; //Maximum of 99
 
 
 void setup(){
+  //Basic setup
   Serial.begin(9600);
   pinMode(DATA, OUTPUT);
   pinMode(LATCH, OUTPUT);
@@ -36,12 +29,11 @@ void setup(){
   pinMode(BRIDGE_OUT, OUTPUT);
   pinMode(BUZZER,OUTPUT);
   pinMode(BRIDGE_IN, INPUT);
-  digitalWrite(BRIDGE_OUT,LOW);
-  analogWrite(BUZZER,0);
+  digitalWrite(BRIDGE_OUT,LOW); //Writes low to the other Arduino so it doesn't think that the game is over yet
 }
 
 void loop(){
-  for (int i = 60; i >= 0; i--)
+  for (int i = timerLength; i >= 0; i--)
   {
     //Shows the countdown to the display
     digitalWrite(LATCH, LOW);
@@ -56,16 +48,17 @@ void loop(){
     Serial.println("1 SECOND");
     delay(1000);
   }
-  //If your time runs out, the code below will run
+  //If your timer runs out, or the for-loop is broken, the code below will run
   
   //Game over
   if(gameWon == false){
-    //
+    //Tell the other Arduino that you lost
     digitalWrite(BRIDGE_OUT, HIGH);
-    analogWrite(BUZZER,255);
+    //Make a buzzing sound
+    tone(BUZZER,2000,1000);
   }
+  //Infinite loop once the game is over, for winning AND losing
   while(true){
-    //wait for gameState to change from "win" to "lose"
     digitalWrite(BRIDGE_OUT,LOW);
     Serial.println("stopwatch done...");
   }
